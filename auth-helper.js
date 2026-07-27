e98ffc1fe871807a4e2011922608f27c84db595d
Obiderick kelvin <obiderickkelvin@gmail.com>
2026-07-27 08:30:04 +0100

    update

--- auth-helper.js
+++ auth-helper.js
@@ -171,7 +171,7 @@ const BlackcessDB = {
        }
        localStorage.removeItem("activeUser");
        if (redirect) {
-           window.location.href = "./admin-login.html";
+           window.location.href = "../admin-login.html";
        }
     },
 
